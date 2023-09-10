9e7951c165a4744adea3f243f491e68585edb6a3
tukamilano <milanotukamoto@g.ecc.u-tokyo.ac.jp>
2023-09-10 15:57:53 +0900

    third commit

--- src/Lean3Example.lean
+++ src/Lean3Example.lean
@@ -2,9 +2,9 @@ open nat
 
 theorem hello_world0 (a b c : ℕ) : a + b + c = a + c + b :=
 begin
-  sorry
+  rw [add_assoc, add_comm b, ←add_assoc]
 end
-
+/-
 theorem hello_world1 (a b c : ℕ) : (a + b) ^ 2 - (a - b) ^ 2 = 4 * a * b :=
 begin
   sorry
@@ -14,3 +14,4 @@ theorem hello_world2 (a b c : ℕ) : (a + b + c) ^ 2 = a ^ 2 + b ^ 2 + c ^ 2 + 2
 begin
   sorry
 end
+-/

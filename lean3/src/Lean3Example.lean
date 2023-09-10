696b582c47d0d4a0c2c3c446986fa4f794dcd2b5
tukamilano <milanotukamoto@g.ecc.u-tokyo.ac.jp>
2023-09-10 15:14:05 +0900

    first commit

---
+++ lean3/src/Lean3Example.lean
@@ -0,0 +1,16 @@
+open nat
+
+theorem hello_world0 (a b c : ℕ) : a + b + c = a + c + b :=
+begin
+  sorry
+end
+
+theorem hello_world1 (a b c : ℕ) : (a + b) ^ 2 - (a - b) ^ 2 = 4 * a * b :=
+begin
+  sorry
+end
+
+theorem hello_world2 (a b c : ℕ) : (a + b + c) ^ 2 = a ^ 2 + b ^ 2 + c ^ 2 + 2 * a * b + 2 * b * c + 2 * c * a :=
+begin
+  sorry
+end

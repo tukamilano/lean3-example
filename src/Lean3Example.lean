f4996e73121d9d7109e3cd5f5e837d7cd666a6ee
tukamilano <milanotukamoto@g.ecc.u-tokyo.ac.jp>
2023-09-12 12:52:15 +0900

    名前の違いを修正

--- src/Lean3Example.lean
+++ src/Lean3Example.lean
@@ -4,14 +4,3 @@ theorem hello_world0 (a b c : ℕ) : a + b + c = a + c + b :=
 begin
   rw [add_assoc, add_comm b, ←add_assoc]
 end
-/-
-theorem hello_world1 (a b c : ℕ) : (a + b) ^ 2 - (a - b) ^ 2 = 4 * a * b :=
-begin
-  sorry
-end
-
-theorem hello_world2 (a b c : ℕ) : (a + b + c) ^ 2 = a ^ 2 + b ^ 2 + c ^ 2 + 2 * a * b + 2 * b * c + 2 * c * a :=
-begin
-  sorry
-end
--/

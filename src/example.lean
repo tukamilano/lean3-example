3819d6ea60fdc18b0fea763661bf44f0e731ea87
tukamilano <milanotukamoto@g.ecc.u-tokyo.ac.jp>
2023-09-15 14:23:04 +0900

    問題の追加

--- src/example.lean
+++ src/example.lean
@@ -5,18 +5,18 @@ begin
   sorry
 end
 
-theorem hello_world1 (a b c : ℕ) : (a + b) ^ 2 = a ^ 2 + 2 * a * b + b ^ 2 :=
+theorem hello_world1 (a b c : ℕ) : (a + b) * (a + b) = a * a + 2 * a * b + b * b :=
 begin
   sorry
 end
 
 
-theorem hello_world2 (a b c : ℕ) : (a + b) ^ 2 - (a - b) ^ 2 = 4 * a * b :=
+theorem hello_world2 (a b c : ℕ) : (a + b) * (a + b) - (a - b) * (a - b) = 4 * a * b :=
 begin
   sorry
 end
 
-theorem hello_world3 (a b c : ℕ) : (a + b + c) ^ 2 = a ^ 2 + b ^ 2 + c ^ 2 + 2 * a * b + 2 * b * c + 2 * c * a :=
+theorem hello_world3 (a b c : ℕ) : (a + b + c) * (a + b + c) = a * a + b * b + c * c + 2 * a * b + 2 * b * c + 2 * c * a :=
 begin
   sorry
 end

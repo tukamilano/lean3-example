cb9ae22a1cb71804e133a50b27366ef3913d0e65
tukamilano <milanotukamoto@g.ecc.u-tokyo.ac.jp>
2023-09-15 14:20:33 +0900

    問題の追加

--- src/example.lean
+++ src/example.lean
@@ -2,5 +2,21 @@ open nat (add_assoc add_comm)
 
 theorem hello_world0 (a b c : ℕ) : a + b + c = a + c + b :=
 begin
-  rw [add_assoc, add_comm b, ←add_assoc]
+  sorry
+end
+
+theorem hello_world1 (a b c : ℕ) : (a + b) ^ 2 = a ^ 2 + 2 * a * b + b ^ 2 :=
+begin
+  sorry
+end
+
+
+theorem hello_world2 (a b c : ℕ) : (a + b) ^ 2 - (a - b) ^ 2 = 4 * a * b :=
+begin
+  sorry
+end
+
+theorem hello_world3 (a b c : ℕ) : (a + b + c) ^ 2 = a ^ 2 + b ^ 2 + c ^ 2 + 2 * a * b + 2 * b * c + 2 * c * a :=
+begin
+  sorry
 end
